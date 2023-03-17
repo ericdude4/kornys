48143628190c6f12b46797ab255d37af37844b5c
ericdude4 <eric_froese2@hotmail.com>
2023-03-17 18:11:34 -0400

    render duplicate skus in a data table

--- src/components/ChooseSyncField.tsx
+++ src/components/ChooseSyncField.tsx
@@ -1,9 +1,10 @@
-import { FormLayout, Checkbox, TextField, Button, InlineError, CalloutCard, LegacyStack, Text, Divider, AlphaStack, Collapsible, TextContainer, ButtonGroup, RadioButton } from '@shopify/polaris';
-import { type } from 'os';
+import {
+    IndexTable, LegacyCard, useIndexResourceState, Button, Text, Divider, AlphaStack, Collapsible, ButtonGroup, RadioButton, Pagination, Columns
+} from '@shopify/polaris';
 import { useState, useCallback } from 'react';
 import { useNavigate, useRouteLoaderData } from 'react-router-dom';
-import { storeHost } from '../utils';
-import Login from './Login';
+import { get } from '../fetch';
+import { storeHost, truncate } from '../utils';
 
 export default function ChooseSyncField() {
     const store: any = useRouteLoaderData("root");
@@ -16,7 +17,61 @@ export default function ChooseSyncField() {
         [],
     );
 
+    const [checking, setChecking] = useState(false);
+
+    const [open, setOpen] = useState(false);
+
+    const resourceName = {
+        singular: 'variant',
+        plural: 'variants',
+    };
+
+    const [variants, setVariants] = useState([]);
+
+    const { selectedResources, allResourcesSelected, handleSelectionChange } =
+        useIndexResourceState(variants);
+
+    const checkForDuplicates = () => {
+        setChecking(true)
+        setOpen(true)
+
+        get("/stores/" + storeHost(store.url) + "/duplicate_sync_fields?sync_property=" + value + "&page=" + 0)
+            .then(variants_page => {
+                setChecking(false)
+                setVariants(variants_page.data)
+            });
+    }
+
     const syncPropertyMarkup = (<strong>{value == 'sku' ? 'SKU' : 'barcode'}</strong>)
+
+
+    const rowMarkup = variants.map(
+        ({ id, product, title, sku, barcode }: any, index) => (
+            <IndexTable.Row
+                id={id}
+                key={id}
+                selected={selectedResources.includes(id)}
+                position={index}
+            >
+                <IndexTable.Cell>
+                    <Text fontWeight="bold" as="span">
+                        {truncate(product.title, 50)}
+                    </Text>
+                </IndexTable.Cell>
+                <IndexTable.Cell>{title}</IndexTable.Cell>
+                <IndexTable.Cell>
+                    <Text as="span" alignment="start" numeric>
+                        {sku}
+                    </Text>
+                </IndexTable.Cell>
+                <IndexTable.Cell>
+                    <Text as="span" alignment="start" numeric>
+                        {barcode}
+                    </Text>
+                </IndexTable.Cell>
+            </IndexTable.Row>
+        ),
+    );
 
     return (
         <AlphaStack gap="4">
@@ -43,12 +98,72 @@ export default function ChooseSyncField() {
             />
             <RadioButton
                 label="Barcode"
-                helpText="Synkro will compare your variant SKUs to determine which variants to sync."
+                helpText="Synkro will compare your variant barcodes to determine which variants to sync."
                 id="barcode"
                 name="barcode"
                 checked={value === 'barcode'}
                 onChange={handleChange}
             />
-        </AlphaStack>
+
+            {checking ? (
+                <ButtonGroup>
+                    <Button primary disabled>
+                        Checking for duplicated {value == 'sku' ? 'SKU' : 'barcode'}s within your store...
+                    </Button>
+                </ButtonGroup>
+            ) : (
+                <Columns gap="4" columns={['twoThirds', 'oneThird']}>
+                    <ButtonGroup>
+                        <Button primary onClick={checkForDuplicates}>
+                            Check for duplicated {value == 'sku' ? 'SKU' : 'barcode'}s within your store
+                        </Button>
+                    </ButtonGroup>
+
+                    {variants.length > 0 ? (
+                        <div id='duplicate-sku-pagination'>
+                            <Pagination
+                                hasPrevious
+                                onPrevious={() => {
+                                    console.log('Previous');
+                                }}
+                                hasNext
+                                onNext={() => {
+                                    console.log('Next');
+                                }}
+                            />
+                        </div>
+                    ) : null}
+                </Columns>
+            )}
+
+            <Divider borderStyle="base" />
+
+            <Collapsible
+                open={open}
+                id="basic-collapsible"
+                transition={{ duration: '100ms', timingFunction: 'ease-in-out' }}
+                expandOnPrint
+            >
+                <LegacyCard>
+                    <IndexTable
+                        resourceName={resourceName}
+                        itemCount={variants.length}
+                        selectedItemsCount={
+                            allResourcesSelected ? 'All' : selectedResources.length
+                        }
+                        onSelectionChange={handleSelectionChange}
+                        headings={[
+                            { title: 'Product' },
+                            { title: 'Variant' },
+                            { title: 'SKU' },
+                            { title: 'Barcode' },
+                        ]}
+                    >
+                        {rowMarkup}
+                    </IndexTable>
+                </LegacyCard>
+            </Collapsible>
+
+        </AlphaStack >
     );
 }
